c56e40203162e9c0050a3aeae109be30b5a56e81
okotohime <okotohime@outlook.com>
2026-05-21 18:44:14 +0800

    solve section {clippy, conversions, macros, tests}

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+        let mut svec = s.split(',');
+        if let Some(name) = svec.next() {
+            if name.is_empty() {
+                return Person::default();
+            }
+            if let Some(age) = svec.next() {
+                if let Ok(age) = age.parse::<usize>() {
+                    if svec.next().is_none() {
+                        return Person {
+                            name: name.to_string(),
+                            age,
+                        };
+                    }
+                }
+            }
+        }
+        Person::default()
     }
 }
 
